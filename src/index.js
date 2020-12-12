526c485eb66e0426d3835c2d0f7d4460d07d5a96
Sean <sean@vigorwebsolutions.com>
2020-12-11 17:59:16 -0800

    Updates

--- src/index.js
+++ src/index.js
@@ -18,7 +18,7 @@ const Container = styled.div`
 const Card = styled.div`
   width: min(calc(55vh - 40px), calc(100vw - 40px));
   height: min(calc(100vh - 40px), calc(181vw - 40px));
-  background: #ffffff;
+  background: #F5F5F7;
   border-radius: 17px;
   overflow: hidden;
 `;
@@ -29,64 +29,92 @@ const CardContent = styled.div`
   position: relative;
 `;
 
-const ImageHeader = styled.div`
+const Image = styled.div`
   position: relative;
   height: 100%;
   width: 100%;
-  background: ${({ style: { backgroundColor } }) => backgroundColor || '#ffffff'} url(${({ style: { image } }) => image}) top center / contain no-repeat;
+  background: url(${({ style: { image } }) => image}) center center / cover no-repeat;
   cursor: ${({ style: { showPointer } }) => showPointer ? 'pointer' : 'initial'};
 `;
 
 const Controls = styled.div`
   display: flex;
   align-items: center;
-  justify-content: space-between;
-  height: 72px;
-  width: calc(100% - 32px);
-  padding: 0 16px;
+  flex-direction: column;
+  width: 100%;
   position: absolute;
   bottom: 0;
   left: 0;
 `;
 
 const NextButton = styled.button`
-  align-items: center;
-  appearance: none;
-  background: none;
-  border-radius: 6px;
-  border: 2px solid #000;
-  bottom: 15px;
+  border: 0;
+  outline: 0;
+  border-radius: 4px;
+  font-size: 16px;
+  width: calc(100% - 48px);
+  padding: 16px 0;
   cursor: pointer;
   display: flex;
   font-family: -apple-system,BlinkMacSystemFont,"Segoe UI",Roboto,Helvetica,Arial,sans-serif,"Apple Color Emoji","Segoe UI Emoji","Segoe UI Symbol";
   font-weight: bold;
-  height: 43px;
   justify-content: center;
-  min-width: 98px;
-  outline: none;
-  padding: 0 10px;
-  text-transform: uppercase;
-  transition: 0.1s ease transform;
+  background: #267afd;
+  color: #ffffff;
 
-  background-color: ${({ style: { isRemixButton } }) => isRemixButton ? 'rgb(0, 0, 0)' : 'none'};
-  color: ${({ style: { isRemixButton } }) => isRemixButton ? 'rgb(255, 255, 255)' : '#000000'};
+  @media screen and (max-height: 568px) {
+    font-size: 14px;
+    padding: 12px 0;
+  }
+
+  @media screen and (max-height: 524px) {
+    font-size: 14px;
+    padding: 8px 0;
+  }
+`;
+
+const SkipButton = styled.button`
+  font-size: 16px;
+  font-weight: bold;
+  justify-content: center;
+  background: transparent;
+  color: #267afd;
+  border: 0;
+  outline: 0;
+  width: calc(100% - 48px);
+  padding: 16px 0;
+  margin: 2px auto 4px auto;
+  cursor: pointer;
+
+  @media screen and (max-height: 568px) {
+    font-size: 14px;
+    padding: 12px 0;
+  }
 `;
 
 const Indicators = styled.div`
-  height: 96px;
   display: flex;
   flex-direction: row;
   align-items: center;
+  margin-bottom: 28px;
+
+  @media screen and (max-height: 568px) {
+    margin-bottom: 26px;
+  }
+
+  @media screen and (max-height: 524px) {
+    margin-bottom: 18px;
+  }
 `;
 
 const Indicator = styled.div`
-  width: 10px;
-  height: 10px;
-  background: #7B7B7B;
+  width: 8px;
+  height: 8px;
+  background: #267afd;
+  opacity: ${({ style: { isActive } }) => isActive ? '1' : '0.4'};
   border-radius: 50%;
-  margin-right: 13px;
-  transition: 0.3s ease transform;
-  transform: scale(${({ style: { isActive } }) => isActive ? '1.5' : '1'});
+  margin: 0 6.5px;
+  transition: 0.3s ease opacity;
   cursor: pointer;
 `;
 
@@ -95,7 +123,7 @@ const ScrollWrapper = styled.div`
   top: 0;
   left: 0;
   width: 100%;
-  height: calc(100% - 72px);
+  height: 100%;
   transition: transform 0.2s ease-in-out;
   transform: ${({ style: { isNext, isPrev } }) => {
     if (isNext) return 'translate(100vw, 0)';
@@ -152,7 +180,7 @@ const VideoWrapper = styled.div`
   }
 `;
 
-const About = ({ cards, onCreateRemix, nextButtonText, remixButtonText }) => {
+const About = ({ cards, onCreateRemix, nextButtonText, remixButtonText, skipButtonText }) => {
   const [cardIndex, setCardIndex] = useState(0);
   const [showOverlay, setShowOverlay] = useState(false);
   const [showOpacity, setShowOpacity] = useState(false);
@@ -212,7 +240,7 @@ const About = ({ cards, onCreateRemix, nextButtonText, remixButtonText }) => {
       <Card>
         <CardContent {...handlers}>
           {
-            cards.map(({ image, mp4, mp4Offset, backgroundColor, videoURL }, idx) => (
+            cards.map(({ image, mp4, mp4Offset, videoURL }, idx) => (
               <ScrollWrapper
                 key={idx}
                 style={{
@@ -221,8 +249,8 @@ const About = ({ cards, onCreateRemix, nextButtonText, remixButtonText }) => {
                   isPrev: cardIndex > idx,
                 }}
               >
-                <ImageHeader
-                  style={{ image, showPointer: videoURL, backgroundColor }}
+                <Image
+                  style={{ image, showPointer: videoURL }}
                 />
                 {
                   (mp4 && mp4 !== '') &&
@@ -258,6 +286,9 @@ const About = ({ cards, onCreateRemix, nextButtonText, remixButtonText }) => {
             >
               {buttonText}
             </NextButton>
+            <SkipButton onClick={() => onCreateRemix()}>
+              {skipButtonText && skipButtonText !== '' ? skipButtonText : 'Skip'}
+            </SkipButton>
           </Controls>
         </CardContent>
       </Card>
@@ -270,11 +301,11 @@ About.propTypes = {
     image: PropTypes.string.isRequired,
     mp4: PropTypes.string,
     mp4Offset: PropTypes.number,
-    backgroundColor: PropTypes.string,
   })),
   nextButtonText: PropTypes.string,
   onCreateRemix: PropTypes.func,
   remixButtonText: PropTypes.string,
+  skipButtonText: PropTypes.string,
 };
 
 About.defaultProps = {
@@ -282,6 +313,7 @@ About.defaultProps = {
   nextButtonText: 'Next',
   onCreateRemix() { },
   remixButtonText: 'Get Started',
+  skipButtonText: 'Skip',
 };
 
 export default About;
